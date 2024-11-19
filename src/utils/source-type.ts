a4c246d212f64159eaf5282eefd6c8c58413ef80
SukkaW <isukkaw@gmail.com>
2024-11-19 11:16:39 +0800

    fix(#185): update sourceType utils

--- src/utils/source-type.ts
+++ src/utils/source-type.ts
@@ -1,10 +1,18 @@
 import type { RuleContext } from '../types'
 
 export default function sourceType(context: RuleContext) {
+  if ('languageOptions' in context && context.languageOptions) {
+    if ('sourceType' in context.languageOptions) {
+      return context.languageOptions.sourceType
+    }
+    if ('parserOptions' in context.languageOptions && context.languageOptions.parserOptions) {
+      if ('sourceType' in context.languageOptions.parserOptions) {
+        return context.languageOptions.parserOptions.sourceType
+      }
+    }
+    return context.languageOptions.sourceType
+  }
   if ('sourceType' in context.parserOptions) {
     return context.parserOptions.sourceType
-  }
-  if ('languageOptions' in context && context.languageOptions) {
-    return context.languageOptions.sourceType
   }
 }
