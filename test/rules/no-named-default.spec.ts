f4e2db1ba1a81c6acdbd80d4c4bbac61e1d800e7
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-11-19 08:35:32 +0100

    test(no-named-default): migrate to `createRuleTestCaseFunctions`

--- test/rules/no-named-default.spec.ts
+++ test/rules/no-named-default.spec.ts
@@ -1,22 +1,29 @@
 import { RuleTester as TSESLintRuleTester } from '@typescript-eslint/rule-tester'
+import { AST_NODE_TYPES } from '@typescript-eslint/utils'
 
-import { test, SYNTAX_VALID_CASES, parsers } from '../utils'
+import {
+  createRuleTestCaseFunctions,
+  SYNTAX_VALID_CASES,
+  parsers,
+} from '../utils'
 
 import rule from 'eslint-plugin-import-x/rules/no-named-default'
 
 const ruleTester = new TSESLintRuleTester()
 
+const { tValid, tInvalid } = createRuleTestCaseFunctions<typeof rule>()
+
 ruleTester.run('no-named-default', rule, {
   valid: [
-    test({ code: 'import bar from "./bar";' }),
-    test({ code: 'import bar, { foo } from "./bar";' }),
+    tValid({ code: 'import bar from "./bar";' }),
+    tValid({ code: 'import bar, { foo } from "./bar";' }),
 
     // Should ignore imported flow types
-    test({
+    tValid({
       code: 'import { type default as Foo } from "./bar";',
       languageOptions: { parser: require(parsers.BABEL) },
     }),
-    test({
+    tValid({
       code: 'import { typeof default as Foo } from "./bar";',
       languageOptions: { parser: require(parsers.BABEL) },
     }),
@@ -25,39 +32,45 @@ ruleTester.run('no-named-default', rule, {
   ],
 
   invalid: [
-    /*test({
-      code: 'import { default } from "./bar";',
-      errors: [{
-        message: 'Use default import syntax to import \'default\'.',
-        type: 'Identifier',
-      }],
-        languageOptions: { parser: require(parsers.BABEL) } ,
-    }),*/
-    test({
+    // tInvalid({
+    //   code: 'import { default } from "./bar";',
+    //   errors: [
+    //     {
+    //       messageId: 'default',
+    //       data: { importName: 'default' },
+    //       type: AST_NODE_TYPES.Identifier,
+    //     },
+    //   ],
+    //   languageOptions: { parser: require(parsers.BABEL) },
+    // }),
+    tInvalid({
       code: 'import { default as bar } from "./bar";',
       errors: [
         {
-          message: "Use default import syntax to import 'bar'.",
-          type: 'Identifier',
+          messageId: 'default',
+          data: { importName: 'bar' },
+          type: AST_NODE_TYPES.Identifier,
         },
       ],
     }),
-    test({
+    tInvalid({
       code: 'import { foo, default as bar } from "./bar";',
       errors: [
         {
-          message: "Use default import syntax to import 'bar'.",
-          type: 'Identifier',
+          messageId: 'default',
+          data: { importName: 'bar' },
+          type: AST_NODE_TYPES.Identifier,
         },
       ],
     }),
 
-    test({
+    tInvalid({
       code: 'import { "default" as bar } from "./bar";',
       errors: [
         {
-          message: "Use default import syntax to import 'bar'.",
-          type: 'Identifier',
+          messageId: 'default',
+          data: { importName: 'bar' },
+          type: AST_NODE_TYPES.Identifier,
         },
       ],
       languageOptions: {
