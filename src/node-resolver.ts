be9c3e8c31426a50ac13f77ec8b797780f004e72
Bill Collins <bilbocollins@gmail.com>
2024-12-19 12:06:03 +0000

    fix: move `enhanced-resolve` to prod deps (#211)

--- src/node-resolver.ts
+++ src/node-resolver.ts
@@ -1,30 +1,33 @@
-import { ResolverFactory, CachedInputFileSystem, type ResolveOptions } from 'enhanced-resolve';
-import fs from 'node:fs';
-import type { NewResolver } from './types';
-import { isBuiltin } from 'node:module';
-import { dirname } from 'node:path';
+import fs from 'node:fs'
+import { isBuiltin } from 'node:module'
+import path from 'node:path'
 
-interface NodeResolverOptions extends Omit<ResolveOptions, 'useSyncFileSystemCalls'> {
+import { ResolverFactory, CachedInputFileSystem } from 'enhanced-resolve'
+import type { ResolveOptions } from 'enhanced-resolve'
+
+import type { NewResolver } from './types'
+
+type NodeResolverOptions = {
   /**
    * The allowed extensions the resolver will attempt to find when resolving a module
    * @type {string[] | undefined}
    * @default ['.mjs', '.cjs', '.js', '.json', '.node']
    */
-  extensions?: string[];
+  extensions?: string[]
   /**
    * The import conditions the resolver will used when reading the exports map from "package.json"
    * @type {string[] | undefined}
    * @default ['default', 'module', 'import', 'require']
    */
-  conditionNames?: string[];
-}
+  conditionNames?: string[]
+} & Omit<ResolveOptions, 'useSyncFileSystemCalls'>
 
 export function createNodeResolver({
   extensions = ['.mjs', '.cjs', '.js', '.json', '.node'],
   conditionNames = ['default', 'module', 'import', 'require'],
-  mainFields = ['main'],
-  exportsFields = ['exports'],
-  mainFiles = ['index'],
+  mainFields: _mainFields = ['main'],
+  exportsFields: _exportsFields = ['exports'],
+  mainFiles: _mainFiles = ['index'],
   fileSystem = new CachedInputFileSystem(fs, 4 * 1000),
   ...restOptions
 }: Partial<NodeResolverOptions> = {}): NewResolver {
@@ -34,7 +37,7 @@ export function createNodeResolver({
     conditionNames,
     useSyncFileSystemCalls: true,
     ...restOptions,
-  });
+  })
 
   // shared context across all resolve calls
 
@@ -43,26 +46,26 @@ export function createNodeResolver({
     name: 'eslint-plugin-import-x built-in node resolver',
     resolve: (modulePath, sourceFile) => {
       if (isBuiltin(modulePath)) {
-        return { found: true, path: null };
+        return { found: true, path: null }
       }
 
       if (modulePath.startsWith('data:')) {
-        return { found: true, path: null };
+        return { found: true, path: null }
       }
 
       try {
-        const path = resolver.resolveSync(
+        const resolved = resolver.resolveSync(
           {},
-          dirname(sourceFile),
-          modulePath
-        );
-        if (path) {
-          return { found: true, path };
+          path.dirname(sourceFile),
+          modulePath,
+        )
+        if (resolved) {
+          return { found: true, path: resolved }
         }
-        return { found: false };
+        return { found: false }
       } catch {
-        return { found: false };
+        return { found: false }
       }
-    }
+    },
   }
 }
