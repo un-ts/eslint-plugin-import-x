1e09f8bc4e81969431c4211e8e4e8bf08fd20dbb
JounQin <admin@1stg.me>
2025-05-31 08:26:45 +0800

    test: add test case for the error message

--- src/rules/no-unused-modules.ts
+++ src/rules/no-unused-modules.ts
@@ -67,7 +67,7 @@ function listFilesUsingFileEnumerator(
     ) {
       throw new Error(`
 Due to the exclusion of certain internal ESLint APIs when using flat config,
-the import/no-unused-modules rule requires an .eslintrc file to know which
+the import-x/no-unused-modules rule requires an .eslintrc file (even empty) to know which
 files to ignore (even when using flat config).
 The .eslintrc file only needs to contain "ignorePatterns", or can be empty if
 you do not want to ignore any files.
