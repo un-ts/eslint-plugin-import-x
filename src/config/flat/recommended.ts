049554a95f77fdc738c15443ca9f50a506999420
José Miguel Gonçalves <jose.goncalves@inov.pt>
2025-06-03 16:45:00 +0100

    fix: drop languageOptions config from flat/recommended
    This will prevent showing linting errors on code that relies on ECMAScript features implemented after 2018.
    If a user wants to limit the ECMAScript version to use, it should set that on his config.

--- src/config/flat/recommended.ts
+++ src/config/flat/recommended.ts
@@ -15,11 +15,4 @@ export default {
     'import-x/no-named-as-default-member': 'warn',
     'import-x/no-duplicates': 'warn',
   },
-
-  // need all these for parsing dependencies (even if _your_ code doesn't need
-  // all of them)
-  languageOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
 } satisfies PluginFlatBaseConfig
