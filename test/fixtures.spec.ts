3ea730f675a5f43b818fd6e32d9f240f637e0d06
JounQin <admin@1stg.me>
2025-06-19 07:55:58 +0800

    test: only run pnp case for `test-compiled` (#389)

--- test/fixtures.spec.ts
+++ test/fixtures.spec.ts
@@ -2,9 +2,11 @@ import path from 'node:path'
 
 import { exec } from 'tinyexec'
 
+import { testCompiled } from './utils.js'
+
 const TIMEOUT = 60_000
 
-describe('yarn pnp', () => {
+;(testCompiled ? describe : describe.skip)('yarn pnp', () => {
   const yarnPnpDir = path.resolve('test/fixtures/yarn-pnp')
   const execOptions = { nodeOptions: { cwd: yarnPnpDir } }
 
